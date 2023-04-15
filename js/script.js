93ce1593a5b356ca884cdae7a68d2b16752b74c0
Kauanzera <kauan_falima@hotmail.com>
2023-04-15 17:58:53 -0300

    landingpage

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/*====== toogle icon navbar ======*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+/*====== scroll sections active link ======*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < (offset + height)) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      })
+    }
+  })
+}
+
+/*====== sticky navbar ======*/
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+/*======remove toggle icon and navbar when click navbar link(scroll) ======*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
+/*====== scroll reveal ======*/
+ScrollReveal({
+  reset: false,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-txt h1, .menu h1, #contact h1', { origin: 'top' });
+ScrollReveal().reveal('.menu h2, .btn, .menu-interno h3', { origin: 'bottom' });
+ScrollReveal().reveal('.home-txt h2, .menu-interno h4, .contact-ifood', { origin: 'left' });
+ScrollReveal().reveal('.home-txt p, .menu-interno p, .contact-info', { origin: 'right' });
+
